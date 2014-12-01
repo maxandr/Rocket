ef864ae9ceb373e1c242429b007efaf00a6f897b
maxandr <maxandryo@gmail.com>
2014-12-01 16:00:00 +0900

    1

---
+++ Assets/Scripts/RocketBehavior.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketBehavior : MonoBehaviour
+{
+		public int forcex;
+		public int forcey;
+		public Transform COM;
+		// Use this for initialization
+		void Start ()
+		{
+				Debug.Log ("1");
+				int a = 5;
+				//rigidbody2D.centerOfMass = new Vector3 (-1000, -1000, 0);
+				rigidbody2D.centerOfMass = COM.localPosition;
+
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		public void WTF ()
+		{
+				rigidbody2D.AddForce (new Vector2 (forcex, forcey));
+				Debug.Log ("asd");
+		}
+
+}
+
