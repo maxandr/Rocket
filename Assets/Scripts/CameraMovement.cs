0bb528e3c7d1c2a8c9e333f39fc2a763e6b522f1
maxandr <maxandryo@gmail.com>
2014-12-05 06:40:23 +0900

    zoom

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -11,28 +11,33 @@ public class CameraMovement : MonoBehaviour
 		public float minPosY = -10;
 		public float maxPosY = 10;
 		public float dist;
+		int moveFingerId = -1;
 
 		void Start ()
 		{
 		}
+
 		void Update ()
 		{
+#if !UNITY_EDITOR
 				Ray ray1 = Camera.main.ScreenPointToRay (Input.mousePosition);
 				Ray ray2 = Camera.main.ScreenPointToRay (Input.mousePosition);
-#if !UNITY_EDITOR
 				if (Input.touchCount == 1) {
 						Touch touch = Input.touches [0];
 						if (touch.phase == TouchPhase.Began && Input.touches.Length == 1) {
 								Vector2 worldPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 								RaycastHit2D hit = Physics2D.Raycast (worldPoint, Vector2.zero);
-								if (hit.collider == null || hit.collider.tag != "Player" ) {
+								if (hit.collider == null || hit.collider.tag != "Player") {
 										startpoint = ray1.GetPoint (10); // Returns a point at distance units along the ray
 										startpoint.z = -10; // fix z to 0
 										panning = true;
+										moveFingerId = touch.fingerId;
+								} else {
+										return;
 								}
 						}
 		
-						if (panning) {
+						if (panning && moveFingerId == touch.fingerId) {
 								dist = Mathf.Clamp ((Vector3.Distance (endpoint, startpoint)), 0.0f, 1.0f);
 								endpoint = ray2.GetPoint (10); // Returns a point at distance units along the ray
 								endpoint.z = 0; // fix z, somehow its not always 0?
@@ -49,38 +54,43 @@ public class CameraMovement : MonoBehaviour
 		
 						if (touch.phase == TouchPhase.Ended && Input.touches.Length == 1) {
 								panning = false;
-						}
+								moveFingerId=-1;
+						}			
+							
 				}
 #else
-						if (Input.GetMouseButtonDown (0)) {
-								Vector2 worldPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-								RaycastHit2D hit = Physics2D.Raycast (worldPoint, Vector2.zero);
-								if (hit.collider == null || hit.collider.tag != "Player" ) {
-										startpoint = ray1.GetPoint (10); 
-										startpoint.z = -10;
-										panning = true;
-								} 
+				Ray ray1 = Camera.main.ScreenPointToRay (Input.mousePosition);
+				Ray ray2 = Camera.main.ScreenPointToRay (Input.mousePosition);
+				if (Input.GetMouseButtonDown (0)) {
+						Vector2 worldPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+						RaycastHit2D hit = Physics2D.Raycast (worldPoint, Vector2.zero);
+						if (hit.collider == null || hit.collider.tag != "Player") {
+								startpoint = ray1.GetPoint (10); 
+								startpoint.z = -10;
+								panning = true;
+						} else {
+								return;
 						}
-		
-						if (panning) {
-								dist = Mathf.Clamp ((Vector3.Distance (endpoint, startpoint)), 0.0f, 1.0f);
-								endpoint = ray2.GetPoint (10); 
-								endpoint.z = -10; 
+				}
+				if (panning) {
+						dist = Mathf.Clamp ((Vector3.Distance (endpoint, startpoint)), 0.0f, 1.0f);
+						endpoint = ray2.GetPoint (10); 
+						endpoint.z = -10; 
 			
-								if (dist >= 0.1) {
-										transform.position += startpoint - endpoint;
-										Vector3 tNewPos;
-										tNewPos.x = Mathf.Clamp (transform.position.x, minPosX, maxPosX);
-										tNewPos.y = Mathf.Clamp (transform.position.y, minPosY, maxPosY);
-										tNewPos.z = -10;
-										transform.position = tNewPos;
-								}
+						if (dist >= 0.1) {
+								transform.position += startpoint - endpoint;
+								Vector3 tNewPos;
+								tNewPos.x = Mathf.Clamp (transform.position.x, minPosX, maxPosX);
+								tNewPos.y = Mathf.Clamp (transform.position.y, minPosY, maxPosY);
+								tNewPos.z = -10;
+								transform.position = tNewPos;
 						}
+				}
 		
 		
-						if (Input.GetMouseButtonUp (0)) {
-								panning = false;
-						}
+				if (Input.GetMouseButtonUp (0)) {
+						panning = false;
+				}
 #endif
 		}
 
---
+++ Assets/Scripts/PinchZoom.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinchZoom : MonoBehaviour
+{
+		public float perspectiveZoomSpeed = 0.005f;
+		public float orthoZoomSpeed = 0.005f;
+		public float MINZOOM = 6.2f;
+		public float MAXZOOM = 12.0f;
+	
+		void Update ()
+		{
+				if (Input.touchCount == 2) {
+						Touch touchZero = Input.GetTouch (0);
+						Touch touchOne = Input.GetTouch (1);
+			
+						Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+						Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+			
+						float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+						float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+			
+						float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+			
+						float temp = camera.orthographicSize;
+						temp += deltaMagnitudeDiff * orthoZoomSpeed;
+				
+						if (temp < MINZOOM || temp > MAXZOOM) {
+								return;
+						}
+						camera.orthographicSize = temp;
+						camera.orthographicSize = Mathf.Max (camera.orthographicSize, 0.1f);
+				}
+		}
+}
