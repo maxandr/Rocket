a2ea65a0b362bfb48f3be34362076f012f87df17
maxandr <maxandryo@gmail.com>
2014-12-16 14:51:51 +0900

    ufo with light & cow=)))

---
+++ Assets/Scripts/Light.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Light : MonoBehaviour {
+	public GameObject lightScaler ;
+	public GameObject light ;
+	BoxCollider2D boxCollider2D;
+	// Use this for initialization
+	void Start () {
+		boxCollider2D = light.GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float h = boxCollider2D.size.y;
+		Vector3 t = lightScaler.transform.localScale;
+		Vector3 t1 = transform.position;
+		t.y = transform.position.y/(h*transform.localScale.y);
+		lightScaler.transform.localScale=t;
+		t1.y -= transform.position.y/2;
+		lightScaler.transform.position=t1;
+	}
+}
